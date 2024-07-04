0902d6018eb2345e6b5626b938c7b0a24efd9a21
Pizza <wilderpizza99@gmail.com>
2024-07-04 18:12:20 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,16 +5,16 @@ arg1=$2
 arg2=$2
 
 execute() {
-  substring="The requested URL returned error"
+  substring="#!/bin/bash"
   sha=$(curl -sSL https://api.github.com/repos/WildePizza/ollama-kubernetes/commits?per_page=2 | jq -r '.[1].sha')
   url="https://raw.githubusercontent.com/WildePizza/ollama-kubernetes/HEAD/.commits/$sha/scripts/$action.sh"
   echo "Executing: $url"
   output=$(curl -fsSL $url 2>&1)
   if [[ $output =~ $substring ]]; then
+    curl -fsSL $url | bash -s $sha $arg1 $arg2
+  else
     sleep 1
     execute
-  else
-    curl -fsSL $url | bash -s $sha $arg1 $arg2
   fi
 }
 execute
