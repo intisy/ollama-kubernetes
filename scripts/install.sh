c95cf0594ee016dfa68c18acf0db5709ce4beed0
Pizza <wilderpizza99@gmail.com>
2024-07-04 18:48:44 +0200

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,6 +3,18 @@
 sha=$1
 using_kubernetes=true
 
+wait_until_ready() {
+  url=$1
+  substring1="The requested URL returned error"
+  substring2="Could not resolve host: raw.githubusercontent.com"
+  echo "Executing: $url"
+  output=$(curl -fsSL $url 2>&1)
+  if [[ $output =~ $substring1 || $output =~ $substring2 ]]; then
+    sleep 1
+    wait_until_ready
+  fi
+}
+wait_until_ready https://raw.githubusercontent.com/WildePizza/ollama-kubernetes/HEAD/.commits/$sha/scripts/deinstall.sh
 curl -fsSL https://raw.githubusercontent.com/WildePizza/ollama-kubernetes/HEAD/.commits/$sha/scripts/deinstall.sh | bash -s
 sudo mkdir ~/ollama
 cd ~/ollama
