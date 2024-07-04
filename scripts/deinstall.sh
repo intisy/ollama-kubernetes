9eb77f13d76a95f4f42b45dd444b2827c0ba1e8a
Pizza <105643708+WildePizza@users.noreply.github.com>
2024-07-04 09:54:37 +0200

    Update deinstall.sh

--- scripts/deinstall.sh
+++ scripts/deinstall.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-
-kubectl delete pv ollamaa-pv --grace-period=0 --force
-kubectl delete pvc ollama-pv-claim --grace-period=0 --force
-kubectl delete deployment ollama-webui --grace-period=0 --force
 kubectl delete service ollama-webui --grace-period=0 --force
+kubectl delete deployment ollama-webui --grace-period=0 --force
+kubectl delete pvc ollama-pv-claim --grace-period=0 --force
+kubectl delete pv ollamaa-pv --grace-period=0 --force
