8ec9358d549d41abb7ecc7ceebc200d8ad701e82
Pizza <105643708+WildePizza@users.noreply.github.com>
2024-07-05 10:52:41 +0200

    Update deinstall.sh

--- scripts/deinstall.sh
+++ scripts/deinstall.sh
@@ -2,5 +2,6 @@
 
 kubectl delete service ollama-webui --grace-period=0 --force
 kubectl delete deployment ollama-webui --grace-period=0 --force
+kubectl delete pods -l app=ollama-webui --grace-period=0 --force
 kubectl delete pvc ollama-pv-claim --grace-period=0 --force
 kubectl delete pv ollama-pv --grace-period=0 --force
